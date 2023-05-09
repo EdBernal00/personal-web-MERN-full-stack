7a46f098c5b98ffbd734828495319b36c9f78b46
EdBernal00 <edbernal00@gmail.com>
2023-05-09 10:58:49 -0400

    add http server with express

---
+++ server/app.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const { API_VERSION } = require("./constants");
+
+const app = express();
+
+// Import routings
+// ...
+
+// Configure Body Parse
+// ...
+
+// Configure Header HTTP - CORS
+// ...
+
+// Configure routings
+// ...
+
+module.exports = app;
