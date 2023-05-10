1eada2b6050c84d4700195737390cce71e26a1e5
EdBernal00 <edbernal00@gmail.com>
2023-05-09 23:26:16 -0400

    add user register

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,9 +1,33 @@
-function register(req, res) {
-  console.log("Se ha ejecutado el registro");
+const bcrypt = require("bcryptjs");
+const User = require("../models/user");
 
-  res.status(200).send({ msg: "TODO OK" });
+function register(req, res) {
+  const { firstName, lastName, email, password } = req.body;
+
+  if (!email) res.status(400).send({ msg: "El email es obligatorio" });
+  if (!password) res.status(400).send({ msg: "La contraseña es obligatoria" });
+
+  const user = new User({
+    firstName,
+    lastName,
+    email: email.toLowerCase(),
+    role: "user",
+    active: false,
+  });
+
+  const salt = bcrypt.genSaltSync(10);
+  const hashPassword = bcrypt.hashSync(password, salt);
+  user.password = hashPassword;
+
+  user.save((error, userStorage) => {
+    if (error) {
+      res.status(400).send({ msg: "Error al crear el usuario" });
+    } else {
+      res.status(200).send(userStorage);
+    }
+  });
 }
 
 module.exports = {
-    register,
-}
+  register,
+};
