53b2b9182bad47cf520c69c193fbda0785fe7576
EdBernal00 <edbernal00@gmail.com>
2023-06-19 11:45:15 -0400

    add creating users

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,3 +1,4 @@
+const bcrypt = require("bcryptjs");
 const User = require("../models/user");
 
 async function getMe(req, res) {
@@ -25,7 +26,30 @@ async function getUsers(req, res) {
   res.status(200).send({ response });
 }
 
+async function createUser(req, res) {
+  const { password } = req.body;
+  const user = new User({ ...req.body, active: false });
+
+  const salt = bcrypt.genSaltSync(10);
+  const hashPassword = bcrypt.hashSync(password, salt);
+  user.password = hashPassword;
+
+  if (req.files.avatar) {
+    // TODO: Upload avatar
+    console.log("Procesando avatar...");
+  }
+
+  user.save((error, userStorage) => {
+    if (error) {
+      res.status(400).send({ msg: "Error al crear el usuario." });
+    } else {
+      res.status(200).send(userStorage);
+    }
+  });
+}
+
 module.exports = {
   getMe,
   getUsers,
+  createUser,
 };
--- server/router/user.js
+++ server/router/user.js
@@ -1,10 +1,13 @@
 const express = require("express");
+const multipart = require("connect-multiparty");
 const UserController = require("../controllers/user");
 const md_auth = require("../middlewares/authenticated");
 
+const md_upload = multipart({ uploadDir: "./uploads/avatar" });
 const api = express.Router();
 
 api.get("/user/me", [md_auth.asureAuth], UserController.getMe);
 api.get("/users", [md_auth.asureAuth], UserController.getUsers);
+api.post("/user", [md_auth.asureAuth, md_upload], UserController.createUser);
 
 module.exports = api;
