4c21e646ca8088da3485e1a276af3a0bcb302a2b
EdBernal00 <edbernal00@gmail.com>
2023-07-02 18:23:36 -0400

    add creating course

--- server/controllers/course.js
+++ server/controllers/course.js
@@ -1,5 +1,21 @@
 const Course = require("../models/course");
+const image = require("../utils/image");
 
-// Functions
+async function createCourse(req, res){
+    const course = new Course(req.body);
 
-module.exports = {};
+    const imagePath = image.getFilePath(req.files.miniature);
+    course.miniature = imagePath;
+
+    course.save((error, courseStorage) => {
+        if(error){
+            res.status(400).send({msg: "Error al crear el curso."});
+        }else{
+            res.status(200).send(courseStorage);
+        }
+    });
+}
+
+module.exports = {
+    createCourse,
+};
--- server/router/course.js
+++ server/router/course.js
@@ -6,6 +6,6 @@ const md_auth = require("../middlewares/authenticated");
 const md_upload = multipart({ uploadDir: "./uploads/course" });
 const api = express.Router();
 
-// APIs
+api.post("/course", [md_auth.asureAuth, md_upload], CourseController.createCourse);
 
 module.exports = api;
