b4c2a8fbde82131ce19ed22dd336751a7765f44b
EdBernal00 <edbernal00@gmail.com>
2023-07-03 12:18:11 -0400

    add updating course

--- server/controllers/course.js
+++ server/controllers/course.js
@@ -1,38 +1,57 @@
 const Course = require("../models/course");
 const image = require("../utils/image");
 
-async function createCourse(req, res){
-    const course = new Course(req.body);
+async function createCourse(req, res) {
+  const course = new Course(req.body);
 
-    const imagePath = image.getFilePath(req.files.miniature);
-    course.miniature = imagePath;
+  const imagePath = image.getFilePath(req.files.miniature);
+  course.miniature = imagePath;
 
-    course.save((error, courseStorage) => {
-        if(error){
-            res.status(400).send({msg: "Error al crear el curso."});
-        }else{
-            res.status(200).send(courseStorage);
-        }
-    });
+  course.save((error, courseStorage) => {
+    if (error) {
+      res.status(400).send({ msg: "Error al crear el curso." });
+    } else {
+      res.status(200).send(courseStorage);
+    }
+  });
 }
 
-async function getCourses(req, res){
-    const {page = 1, limit = 10} = req.query;
-    const options = {
-        page: parseInt(page),
-        limit: parseInt(limit),
-    }
+async function getCourses(req, res) {
+  const { page = 1, limit = 10 } = req.query;
+  const options = {
+    page: parseInt(page),
+    limit: parseInt(limit),
+  };
 
-    Course.paginate({}, options, (error, courses) => {
-        if(error){
-            res.status(400).send({msg: "Error al obtener los cursos."});
-        }else{
-            res.status(200).send(courses);
-        }
-    });
+  Course.paginate({}, options, (error, courses) => {
+    if (error) {
+      res.status(400).send({ msg: "Error al obtener los cursos." });
+    } else {
+      res.status(200).send(courses);
+    }
+  });
+}
+
+function updateCourse(req, res) {
+  const { id } = req.params;
+  const courseData = req.body;
+
+  if (req.files.miniature) {
+    const imagePath = image.getFilePath(req.files.miniature);
+    courseData.miniature = imagePath;
+  }
+
+  Course.findByIdAndUpdate({ _id: id }, courseData, (error, courseUpdate) => {
+    if (error) {
+      res.status(400).send({ msg: "Error al actualizar el curso." });
+    } else {
+      res.status(200).send({ msg: "Actualizado correctamente" });
+    }
+  });
 }
 
 module.exports = {
-    createCourse,
-    getCourses,
+  createCourse,
+  getCourses,
+  updateCourse,
 };
--- server/router/course.js
+++ server/router/course.js
@@ -8,5 +8,6 @@ const api = express.Router();
 
 api.post("/course", [md_auth.asureAuth, md_upload], CourseController.createCourse);
 api.get("/courses", CourseController.getCourses);
+api.patch("/course/:id", [md_auth.asureAuth, md_upload], CourseController.updateCourse);
 
 module.exports = api;
