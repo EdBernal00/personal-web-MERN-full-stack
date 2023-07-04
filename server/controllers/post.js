55e79437fc3369ea321659021a7871d57fd65d3f
EdBernal00 <edbernal00@gmail.com>
2023-07-04 10:57:38 -0400

    add get post by path

--- server/controllers/post.js
+++ server/controllers/post.js
@@ -1,59 +1,87 @@
-const Post = require('../models/post');
-const image = require('../utils/image');
+const Post = require("../models/post");
+const image = require("../utils/image");
 
 function createPost(req, res) {
-    const post = new Post(req.body);
-    post.createdAt = new Date();
+  const post = new Post(req.body);
+  post.createdAt = new Date();
 
-    const imagePath = image.getFilePath(req.files.miniature);
-    post.miniature = imagePath;
+  const imagePath = image.getFilePath(req.files.miniature);
+  post.miniature = imagePath;
 
-    post.save((error, postStorage) => {
-        if (error) {
-            res.status(400).send({ msg: 'Error al crear el post.' });
-        } else {
-            res.status(200).send(postStorage);
-        }
-    });
+  post.save((error, postStorage) => {
+    if (error) {
+      res.status(400).send({ msg: "Error al crear el post." });
+    } else {
+      res.status(200).send(postStorage);
+    }
+  });
 }
 
 function getPosts(req, res) {
-    const { page = 1, limit = 10 } = req.query;
-    const options = {
-        page: parseInt(page),
-        limit: parseInt(limit),
-        sort: { createdAt: 'desc' },
-    }
+  const { page = 1, limit = 10 } = req.query;
+  const options = {
+    page: parseInt(page),
+    limit: parseInt(limit),
+    sort: { createdAt: "desc" },
+  };
 
-    Post.paginate({}, options, (error, posts) => {
-        if (error) {
-            res.status(400).send({ msg: 'Error al obtener los posts.' });
-        } else {
-            res.status(200).send(posts);
-        }
-    });
+  Post.paginate({}, options, (error, posts) => {
+    if (error) {
+      res.status(400).send({ msg: "Error al obtener los posts." });
+    } else {
+      res.status(200).send(posts);
+    }
+  });
 }
 
 function updatePost(req, res) {
-    const { id } = req.params;
-    const postData = req.body;
+  const { id } = req.params;
+  const postData = req.body;
 
-    if (req.files.miniature) {
-        const imagePath = image.getFilePath(req.files.miniature);
-        postData.miniature = imagePath;
+  if (req.files.miniature) {
+    const imagePath = image.getFilePath(req.files.miniature);
+    postData.miniature = imagePath;
+  }
+
+  Post.findByIdAndUpdate({ _id: id }, postData, (error) => {
+    if (error) {
+      res.status(400).send({ msg: "Error al actualizar el post." });
+    } else {
+      res.status(200).send({ msg: "Actualizado correctamente" });
     }
+  });
+}
 
-    Post.findByIdAndUpdate({ _id: id }, postData, (error) => {
-        if (error) {
-            res.status(400).send({ msg: 'Error al actualizar el post.' });
-        } else {
-            res.status(200).send({ msg: 'Actualizado correctamente' });
-        }
-    });
+function deletePost(req, res) {
+  const { id } = req.params;
+
+  Post.findByIdAndDelete({ _id: id }, (error) => {
+    if (error) {
+      res.status(400).send({ msg: "Error al eliminar el post." });
+    } else {
+      res.status(200).send({ msg: "Post eliminado correctamente" });
+    }
+  });
+}
+
+function getPost(req, res) {
+  const { path } = req.params;
+
+  Post.findOne({ path }, (error, post) => {
+    if (error) {
+      res.status(500).send({ msg: "Error del servidor." });
+    } else if (!post) {
+        res.status(404).send({ msg: "Post no encontrado." });
+    } else {
+      res.status(200).send(post);
+    }
+  });
 }
 
 module.exports = {
-    createPost,
-    getPosts,
-    updatePost,
+  createPost,
+  getPosts,
+  updatePost,
+  deletePost,
+  getPost,
 };
--- server/router/post.js
+++ server/router/post.js
@@ -9,5 +9,7 @@ const api = express.Router();
 api.post("/post", [md_auth.asureAuth, md_upload], PostController.createPost);
 api.get("/posts", PostController.getPosts);
 api.patch("/post/:id", [md_auth.asureAuth, md_upload], PostController.updatePost);
+api.delete("/post/:id", [md_auth.asureAuth], PostController.deletePost);
+api.get("/post/:path", PostController.getPost);
 
 module.exports = api;
