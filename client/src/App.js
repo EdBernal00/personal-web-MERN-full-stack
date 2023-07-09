d62c6992d0c2a1af9c4c5558506fec6d4aad7b53
EdBernal00 <edbernal00@gmail.com>
2023-07-09 18:48:28 -0400

    add separating the admin zone from the web zone

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,13 @@
 import React from "react";
-import { Button } from "semantic-ui-react";
+import { BrowserRouter } from "react-router-dom";
+import { WebRouter, AdminRouter } from "./router";
 
 export default function App() {
   return (
-    <div>
-      <h1>Estamos en mi web</h1>
-
-      <Button primary>Primary</Button>
-      <Button secondary>Secondary</Button>
-    </div>
+    <BrowserRouter>
+      <WebRouter />
+      <AdminRouter />
+      
+    </BrowserRouter>
   );
 }
