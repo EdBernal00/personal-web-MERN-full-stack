b9073ed800e7364f927492fc2fc4f164da60946f
EdBernal00 <edbernal00@gmail.com>
2023-07-09 14:42:10 -0400

    add installing semantic-ui-react

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,13 @@
 import React from "react";
+import { Button } from "semantic-ui-react";
 
 export default function App() {
   return (
     <div>
       <h1>Estamos en mi web</h1>
+
+      <Button primary>Primary</Button>
+      <Button secondary>Secondary</Button>
     </div>
   );
 }
