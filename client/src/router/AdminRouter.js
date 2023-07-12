efc1954f906675bfed4f38fb5971e02154dfa461
EdBernal00 <edbernal00@gmail.com>
2023-07-11 23:06:15 -0400

    add layouts to routes

--- client/src/router/AdminRouter.js
+++ client/src/router/AdminRouter.js
@@ -1,11 +1,20 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
+import { AdminLayout } from "../layouts";
 import { Auth } from "../pages/admin";
 
 export function AdminRouter() {
+  const loadLayout = (Layout, Page) => {
+    return (
+      <Layout>
+        <Page />
+      </Layout>
+    );
+  };
+
   return (
     <Routes>
-      <Route path="/admin/*" element={<Auth />} />
+      <Route path="/admin/*" element={loadLayout(AdminLayout, Auth)} />
     </Routes>
   );
 }
