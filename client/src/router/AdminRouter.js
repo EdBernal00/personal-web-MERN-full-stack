f6fc80a2afcc48e9d134d6f97a431280e829693d
EdBernal00 <edbernal00@gmail.com>
2023-07-23 13:33:01 -0400

    add separating the Auth from the AdminPanel

--- client/src/router/AdminRouter.js
+++ client/src/router/AdminRouter.js
@@ -1,7 +1,9 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import { AdminLayout } from "../layouts";
-import { Auth } from "../pages/admin";
+import { Auth, Users } from "../pages/admin";
+
+const user = null;
 
 export function AdminRouter() {
   const loadLayout = (Layout, Page) => {
@@ -14,7 +16,13 @@ export function AdminRouter() {
 
   return (
     <Routes>
-      <Route path="/admin/*" element={loadLayout(AdminLayout, Auth)} />
+      {!user ? (
+        <Route path="/admin/*" element={loadLayout(AdminLayout, Auth)} />
+      ) : (
+        <>
+          <Route path="/admin/users" element={loadLayout(AdminLayout, Users)} />
+        </>
+      )}
     </Routes>
   );
 }
