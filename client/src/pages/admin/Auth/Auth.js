273f5a2e2f56e54b4386b2e567643fe5cabaca13
EdBernal00 <edbernal00@gmail.com>
2023-07-27 16:54:31 -0400

    add register form

--- client/src/pages/admin/Auth/Auth.js
+++ client/src/pages/admin/Auth/Auth.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { Tab } from "semantic-ui-react";
+import { RegisterForm } from "../../../components/Admin/Auth";
 import { Icon } from "../../../assets";
 import "./Auth.scss";
 
@@ -22,7 +23,7 @@ export function Auth() {
       menuItem: "Nuevo usuario",
       render: () => (
         <Tab.Pane>
-          <h2>Register Form</h2>
+          <RegisterForm openLogin={openLogin} />
         </Tab.Pane>
       ),
     },
